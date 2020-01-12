de7470e080389e27c213ba5e2ca0744dc727811c
GreenArrow <1522773921@qq.com>
2020-01-13 01:08:03 +0800

    添加注释

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//包扫描路径
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
 public class GatewayApplication {
